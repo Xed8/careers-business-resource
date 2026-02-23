feea4e98a220aa7c8bf1c9596275b9042119adfe
Xed8 <139227791+Xed8@users.noreply.github.com>
2026-02-23 10:16:22 +0800

    Reduce animation duration for faster mobile loading

--- lib/useScrollAnimation.ts
+++ lib/useScrollAnimation.ts
@@ -11,7 +11,7 @@ interface UseScrollAnimationOptions {
 export function useScrollAnimation(options: UseScrollAnimationOptions = {}) {
     const {
         threshold = 0.1,
-        rootMargin = '0px 0px -100px 0px',
+        rootMargin = '0px 0px -50px 0px',
         triggerOnce = true,
     } = options;
 
